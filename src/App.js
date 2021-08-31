119cc0930243fb948564e8a9ee8a4e9ebd9d66a1
Maede-Dev <85278552+Maede-Dev@users.noreply.github.com>
2021-08-31 19:19:25 +0430

    create some components that we needs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+
+import FooterComponent from './Components/Footer/FooterComponent';
+import Header from './Components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Header />
+        <div className="main-container">
+          <Switch>
+            <Route exact path="/aboutUs" component={AboutUs} />
+            <Route exact path="/productList" component={ProductList} />
+            <Route exact path="/" component={Home} />
+          </Switch>
+        </div>
+        <FooterComponent />
+      </Router>
+    </>
   );
 }
 
---
+++ src/pages/ProductList/ProductList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ProductList = () => {
+    return (
+        <div>
+            this is productList page
+            
+        </div>
+    )
+}
+
+export default ProductList
