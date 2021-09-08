6900feaada1bba63e2655abf575676cda47b817d
mohammad.javadi <mohamadja114@gmail.com>
2021-09-08 11:10:32 +0430

    add sass to project

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,49 +2,46 @@ import { Col, Input, Row, Space } from "antd";
 
 import { Link } from "react-router-dom";
 import React from "react";
+import s from "./Header.scss";
 
 const { Search } = Input;
 const Header = () => {
   return (
-    <Row justify="space-between" align="middle">
-      
-      <Col span={3} className="header-cols">
-      <Link to="/">
-        <span>HOME</span>
+    <Row justify="space-between" align="middle" className={s.header_container}>
+      <Col span={3} className={s.header__cols}>
+        <Link to="/">
+          <span>HOME</span>
         </Link>
       </Col>
-     
-      <Col span={3} className="header-cols">
-      <Link to="/aboutUs">
-        <span>ABOUT US</span>
+
+      <Col span={3} className={s.header__cols}>
+        <Link to="/aboutUs">
+          <span>ABOUT US</span>
         </Link>
       </Col>
-      
-      <Col span={3} className="header-cols">
-      <Link to="/productList">
-        <span>PRODUCT LIST</span>
+
+      <Col span={3} className={s.header__cols}>
+        <Link to="/productList">
+          <span>PRODUCT LIST</span>
         </Link>
       </Col>
-      
-      <Col span={3} className="header-cols">
-      <Link to="/sale">
-        <span>SALE</span>
+
+      <Col span={3} className={s.header__cols}>
+        <Link to="/sale">
+          <span>SALE</span>
         </Link>
       </Col>
-      
+
       <Col span={4} className="header-logo">
-          <img src="/public/Images/Logo.png" alt="header-logo" />
-        </Col>
+        <img src="/public/Images/Logo.png" alt="header-logo" />
+      </Col>
 
-        <Col span={4} className="search">
+      <Col span={4} className="search">
         <Space direction="vertical">
-        <Search placeholder=" search" className="search-box" />
-         </Space>
-        </Col>
-        
+          <Search placeholder=" search" className="search-box" />
+        </Space>
+      </Col>
     </Row>
-
-    
   );
 };
 
