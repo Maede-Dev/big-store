1abe30ecfcaa05b88b2195bf6f3359cec9503194
Maede-Dev <85278552+Maede-Dev@users.noreply.github.com>
2021-09-23 11:14:39 +0330

    complete header and wip

--- Components/ProductListSideBar/index.jsx
+++ Components/ProductListSideBar/index.jsx
@@ -1,21 +1,21 @@
 import React from "react";
-import { Select } from 'antd';
+import { Select } from "antd";
 import style from "./style.module.scss";
 
 const { Option } = Select;
 
 const ProductListSideBar = () => {
-  
-  return <div className={style.sidebar_container}>
-  <Select className={style.select} placeholder="$ price">
-  <Option value="price">$ 25</Option>
-    <Option value="price">$ 52.5</Option>
-    <Option value="price">$ 2.58</Option>
-    <Option value="price">$ 8.96</Option>
-    <Option value="price">$ 5</Option>
-    <Option value="price">$ 78</Option>
-  </Select>
-  </div>;
+  const prices = [25, 45, 2.5, 85.2, 78];
+
+  return (
+    <div className={style.sidebar_container}>
+      <Select className={style.select} placeholder="$ price" allowClear>
+        {prices.map((price) => (
+          <Option value={price}>$ {price}</Option>
+        ))}
+      </Select>
+    </div>
+  );
 };
 
 export default ProductListSideBar;
