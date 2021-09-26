ade178fcd1e516714abb20bf6b998da8109e99af
mohammad.javadi <mohamadja114@gmail.com>
2021-09-26 13:31:01 +0330

    cusomizing scrollbar and move data to different folder

--- pages/product-list/index.jsx
+++ pages/product-list/index.jsx
@@ -4,72 +4,18 @@ import CardItem from "../../Components/Card";
 import ProductListSideBar from "../../Components/ProductListSideBar";
 import React from "react";
 import style from "./productList.module.scss";
+import ExampleList from "../../Components/hardData/productList";
 
 const productList = () => {
-  const exampleList = [
-    {
-      imageAlt: "product1",
-      imageSrc: ["/Images/card1.jpeg"],
-      name: "bag 1",
-      price: "215",
-      colors: ["red", "blue", "black"],
-      id: 2,
-      category: "bag",
-    },
-    {
-      imageAlt: "product2",
-      imageSrc: ["/Images/card2.jpg"],
-      name: "bag 2",
-      price: "165",
-      colors: ["red", "blue", "black"],
-      id: 2,
-      category: "bag",
-    },
-    {
-      imageAlt: "product3",
-      imageSrc: ["/Images/card3.jpg"],
-      name: "bag 3",
-      price: "127",
-      colors: ["red", "blue", "black"],
-      id: 3,
-      category: "bag",
-    },
-    {
-      imageAlt: "product4",
-      imageSrc: ["/Images/card4.jpg", "/Images/card3.jpg"],
-      name: "bag 4",
-      price: "100",
-      colors: ["red", "blue", "black"],
-      id: 4,
-      category: "bag",
-    },
-    {
-      imageAlt: "product5",
-      imageSrc: ["/Images/card5.jpg"],
-      name: "bag 5",
-      price: "79",
-      colors: ["red", "blue", "black"],
-      id: 5,
-      category: "bag",
-    },
-    {
-      imageAlt: "product5",
-      imageSrc: ["/Images/card6.jpg"],
-      name: "bag 6",
-      price: "79",
-      colors: ["red", "blue", "black"],
-      id: 6,
-      category: "bag",
-    },
-  ];
+
   return (
     <Row className={style.cardList_container} justify="space-around">
-      <Col span={5}>
+      <Col span={3}>
         <ProductListSideBar />
       </Col>
-      <Col span={19}>
+      <Col span={20} style={{ borderLeft: "1px solid #afafaf" }}>
         <Row justify="space-around">
-          {exampleList.map((node) => (
+          {ExampleList.map((node) => (
             <CardItem
               key={node.id}
               imageAlt={node.imageAlt}
