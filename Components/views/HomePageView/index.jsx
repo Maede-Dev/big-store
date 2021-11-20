4fb8fbc6b76c9a332c544840119f4e41b1762eb5
mohammad.javadi <mohamadja114@gmail.com>
2021-11-20 22:20:47 +0330

    add api folder

--- Components/views/HomePageView/index.jsx
+++ Components/views/HomePageView/index.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import HomeTop from "../../sections/homeTop";
 import HomeSlider from "../../sections/homeSlider";
 import Brands from "../../sections/Brands";
+import axios from "axios";
 
 const HomePageView = () => {
+  const getUrl = "http://localhost:1337/products";
+  useEffect(() => {
+    axios.get(getUrl).then((res) => console.log("this is res", res.data));
+  }, []);
   return (
     <>
       <HomeTop />
---
+++ back-end/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '800d29b8-31be-49bd-a9e4-5b875439c620'
+};
