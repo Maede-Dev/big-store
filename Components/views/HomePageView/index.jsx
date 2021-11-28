a5fe74bb0d8c8d22b36731662c9cb54569649ea1
Maede-Dev <85278552+Maede-Dev@users.noreply.github.com>
2021-11-28 17:00:31 +0330

    some changes

--- Components/views/HomePageView/index.jsx
+++ Components/views/HomePageView/index.jsx
@@ -1,13 +1,14 @@
 import React, { useEffect } from "react";
-import HomeTop from "../../sections/homeTop";
-import HomeSlider from "../../sections/homeSlider";
+
 import Brands from "../../sections/Brands";
+import HomeSlider from "../../sections/homeSlider";
+import HomeTop from "../../sections/homeTop";
 import axios from "axios";
 
 const HomePageView = () => {
-  // const getUrl = "http://localhost:1337/users/1";
+  const getUrl = "http://localhost:1337/products";
   useEffect(() => {
-    // axios.get(getUrl).then((res) => console.log("this is res", res.data));
+    getData();
     getUser();
   }, []);
   const getUser = async () => {
@@ -18,6 +19,24 @@ const HomePageView = () => {
       })
       .then((res) => console.log(res));
   };
+
+  const getData = async () => {
+    await axios.get(getUrl).then((res) => console.log("this is data", res));
+  };
+
+  // const onFinish = (values) => {
+  //   const query = {
+  //     name: values.name,
+  //     phoneNumber: values.number,
+  //   };
+
+  //   axios
+  //     .post("/ljdflk;ajfl;a/admin", query)
+  //     .then((res) => console.log(res))
+  //     .catch((error) => console.log(error)).finally(
+  //       ....
+  //     );
+  // };
 
   return (
     <>
