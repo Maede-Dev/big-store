c53cdc4a7cc047daca9b3f5a537725acb3780f27
Maede-Karimi <nilaasrae@gmail.com>
2022-02-21 18:19:24 +0330

    show more button

--- pages/product-list/index.jsx
+++ pages/product-list/index.jsx
@@ -1,20 +1,23 @@
 import { Col, Row, Spin } from "antd";
+import React, { useEffect, useState } from "react";
 
 import CardItem from "../../Components/Card";
-import ExampleList from "../../Components/hardData/productList";
-import ProductListSideBar from "../ProductListSideBar";
-import React, { useEffect, useState } from "react";
-import style from "./productList.module.scss";
 import axios from "axios";
+import style from "./productList.module.scss";
 
 const ProductList = () => {
+  const pageSize = 7;
+  const [page, setPage] = useState(1);
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(false);
-  console.log("this is products", products);
+
+  console.log("products length", products.length);
+
   const getProducts = async () => {
     await axios
       .get("http://localhost:1337/products")
-      .then((res) => setProducts(res.data));
+      .then((res) => setProducts(res.data))
+      .catch((error) => console.log(error));
   };
 
   useEffect(() => {
@@ -25,7 +28,7 @@ const ProductList = () => {
     <Spin spinning={loading}>
       <Row className={style.cardList_container} justify="space-around">
         {products?.length > 0 ? (
-          products?.map((node) => (
+          products?.slice(0, pageSize * page)?.map((node) => (
             <Col key={node.id} xs={24} sm={10} md={7} lg={7} xl={5}>
               <CardItem
                 images={node.images}
@@ -41,6 +44,13 @@ const ProductList = () => {
           <div>empty</div>
         )}
       </Row>
+
+      {/* if total(products length) is more/equal than page*pageSize then ... */}
+      {products?.length > 9 && products.length >= page * pageSize && (
+        <div className={style.showMore} onClick={() => setPage(page + 1)}>
+          show more
+        </div>
+      )}
     </Spin>
   );
 };
